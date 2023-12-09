a8126f1273d96b102cca68719ad93a8fe3e340e5
agent19music <seanmotanya@gmail.com>
2023-12-09 11:05:35 +0300

    Done with assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
+  def __init__(self,value=''):
+      self.value = value
   pass
+
+  def get_value(self):
+   return self._value
+  pass
+
+  def set_value(self,value):
+    if not isinstance(value, str):
+      print("The value must be a string.")
+    else:  
+      self._value = value
+
+  value = property(get_value,set_value) 
+
+  def is_sentence(self):
+    return self._value.endswith('.')
+  
+  def is_question(self):
+    return self._value.endswith('?')
+  
+  def is_exclamation(self):
+    return self._value.endswith('!')
+  
+  def count_sentences(self):
+    # Split the string into sentences based on punctuation marks
+    sentences = re.split('[.!?]', self._value)
+    # Filter out any empty strings resulting from the split
+    sentences = list(filter(None, sentences))
+    # Return the count of sentences
+    return len(sentences)
